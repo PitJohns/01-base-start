dc9f296e92c72a13ab4be93b9f6c7061034a5afa
Veniamin Evstratov <dzimmutenno@gmail.com>
2025-02-03 15:26:27 +0500

    end part 1

--- theory.js
+++ theory.js
@@ -34,7 +34,14 @@ null
 //console.log(typeof NaN);
 
 // =========== Priority 
-const = fullAge = 29;
-const = birthYear = 1993;
-const = currentYear = 2023; 
-// > < >= <+
+const  fullAge = 29;
+const  birthYear = 1993;
+const  currentYear = 2023; 
+// > < >= <= 
+/*const isFullAge =( currentYear - birthYear < = fullAge); 
+console.log(isFullAge); */
+
+const num1 = 42;
+const num2 = '42';
+
+console.log( num1 === num2);  
